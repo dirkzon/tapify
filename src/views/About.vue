b095a30ffa9707da378ea45f0afacbcce1723c86
dirk z <dirk.v.zon@outlook.com>
2022-01-15 20:24:28 +0100

    chore: add unversioned files

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: "calback",
+  mounted() {
+
+    this.$store.dispatch("Authorize", {code: this.$route.query.code})
+  }
+};
+</script>
+
