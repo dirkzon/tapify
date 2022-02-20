e2d183b6fb73d3990ee98ef90e8a1fec2ac77b8d
dirk z <dirk.v.zon@outlook.com>
2022-02-20 11:00:16 +0100

    feat: undo track deletion

--- src/components/TrackItem.vue
+++ src/components/TrackItem.vue
@@ -1,6 +1,5 @@
 <template>
-  <v-card
-    :disabled="track.hidden"
+  <div
     id="grad"
     :style="{
       padding: '0px',
@@ -10,40 +9,44 @@
   >
     <v-row style="margin: 1px" :disabled="track.hidden">
       <v-col md="2">
-        <v-avatar tile style="border-radius: 0; width: 60px; height: auto">
-          <v-img :src="track.image"></v-img>
-        </v-avatar>
+        <v-card :disabled="track.hidden" tile flat>
+          <v-avatar tile style="border-radius: 0; width: 60px; height: auto">
+            <v-img :src="track.image"></v-img>
+          </v-avatar>
+        </v-card>
       </v-col>
       <v-col style="margin: 10px">
-        <v-row>
-          <h4 v-text="track.name" style="margin: 0"></h4>
-        </v-row>
-        <v-row
-          v-for="artist in track.artists"
-          :key="artist"
-          style="padding: 2px"
-        >
-          <h6>{{ artist }};</h6>
-        </v-row>
+        <v-card :disabled="track.hidden" flat>
+          <v-row>
+            <h4 v-text="track.name" style="margin: 0"></h4>
+          </v-row>
+          <v-row
+            v-for="artist in track.artists"
+            :key="artist"
+            style="padding: 2px"
+          >
+            <h6>{{ artist }};</h6>
+          </v-row>
+        </v-card>
       </v-col>
       <v-col md="1">
+        <v-btn class="v-icon" icon small @click="hideTrack">
+          <v-icon small v-if="!track.hidden">mdi-delete</v-icon>
+          <v-icon small v-else>mdi-undo</v-icon>
+        </v-btn>
         <v-btn
           :disabled="track.hidden"
           class="v-icon"
           icon
           small
-          @click="hideTrack"
+          @click="lockTrack"
         >
-          <v-icon small v-if="!track.hidden">mdi-delete</v-icon>
-          <v-icon small v-else>mdi-keyboard-return</v-icon>
-        </v-btn>
-        <v-btn class="v-icon" icon small @click="lockTrack">
           <v-icon small v-if="!track.locked">mdi-lock-open</v-icon>
           <v-icon small v-else>mdi-lock</v-icon>
         </v-btn>
       </v-col>
     </v-row>
-  </v-card>
+  </div>
 </template>
 
 <script lang="ts">
